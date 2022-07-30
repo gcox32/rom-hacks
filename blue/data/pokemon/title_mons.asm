9a47d2ae91d4552d4fe0963289bc040f89864a6b
gcox32 <gcoxexcel@gmail.com>
2022-07-30 11:29:56 -0400

    rival team bugfix

--- blue/data/pokemon/title_mons.asm
+++ blue/data/pokemon/title_mons.asm
@@ -1,22 +1,22 @@
 TitleMons:
 ; mons on the title screen are randomly chosen from here
 IF DEF(_RED)
-	db STARTER1
+	db BLASTOISE
 	db STARTER2
-	db STARTER3
-	db WEEDLE
-	db NIDORAN_M
+	db NIDOQUEEN
+	db NIDOKING
+	db ARCANINE
 	db SCYTHER
-	db PIKACHU
-	db CLEFAIRY
+	db MACHAMP
+	db GOLEM
 	db RHYDON
-	db ABRA
-	db GASTLY
-	db DITTO
-	db PIDGEOTTO
-	db ONIX
-	db PONYTA
-	db MAGIKARP
+	db ALAKAZAM
+	db FEAROW
+	db MAGMAR
+	db GOLDUCK
+	db EEVEE
+	db KINGLER
+	db DRAGONITE
 ENDC
 IF DEF(_BLUE)
 	db STARTER2
