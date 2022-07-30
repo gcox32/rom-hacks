e248eec03a444eea3febb0ffb5b215068069b2a0
gcox32 <gcoxexcel@gmail.com>
2022-07-30 11:39:11 -0400

    change blue title mons

--- blue/data/pokemon/title_mons.asm
+++ blue/data/pokemon/title_mons.asm
@@ -1,6 +1,24 @@
 TitleMons:
 ; mons on the title screen are randomly chosen from here
 IF DEF(_RED)
+	db STARTER1
+	db STARTER2
+	db STARTER3
+	db WEEDLE
+	db NIDORAN_M
+	db SCYTHER
+	db PIKACHU
+	db CLEFAIRY
+	db RHYDON
+	db ABRA
+	db GASTLY
+	db DITTO
+	db PIDGEOTTO
+	db ONIX
+	db PONYTA
+	db MAGIKARP
+ENDC
+IF DEF(_BLUE)
 	db BLASTOISE
 	db STARTER2
 	db NIDOQUEEN
@@ -14,25 +32,7 @@ IF DEF(_RED)
 	db FEAROW
 	db MAGMAR
 	db GOLDUCK
-	db EEVEE
-	db KINGLER
+	db DODRIO
+	db PINSIR
 	db DRAGONITE
-ENDC
-IF DEF(_BLUE)
-	db STARTER2
-	db STARTER1
-	db STARTER3
-	db MANKEY
-	db HITMONLEE
-	db VULPIX
-	db CHANSEY
-	db AERODACTYL
-	db JOLTEON
-	db SNORLAX
-	db GLOOM
-	db POLIWAG
-	db DODUO
-	db PORYGON
-	db GENGAR
-	db RAICHU
 ENDC
