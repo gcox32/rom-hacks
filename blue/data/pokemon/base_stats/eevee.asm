b8b2a46d81ebf0700c491a63c64f896d85c64690
Grant Cox <gcoxexcel@gmail.com>
2022-12-19 16:19:18 -0500

    small tweak to eevee in blue

--- blue/data/pokemon/base_stats/eevee.asm
+++ blue/data/pokemon/base_stats/eevee.asm
@@ -10,12 +10,12 @@
 	INCBIN "gfx/pokemon/front/eevee.pic", 0, 1 ; sprite dimensions
 	dw EeveePicFront, EeveePicBack
 
-	db TACKLE, TAIL_WHIP, NO_MOVE, NO_MOVE ; level 1 learnset
+	db TACKLE, TAIL_WHIP, GROWTH, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
 	tmhm TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  RAGE,         \
-	     MIMIC,        DOUBLE_TEAM,  REFLECT,               SWIFT,        \
+	     MIMIC,        DOUBLE_TEAM,  REFLECT,      SWIFT,        \
 	     SKULL_BASH,   REST,         SUBSTITUTE
 	; end
 
