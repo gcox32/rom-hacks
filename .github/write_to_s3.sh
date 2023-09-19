af3bf5ea8907e99fd595acc2d0fbe78c71d6d2aa
Grant Cox <gcoxexcel@gmail.com>
2023-09-19 08:52:49 -0400

    maybe found the bug

--- .github/write_to_s3.sh
+++ .github/write_to_s3.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 S3_BUCKET="letmedemo-assets/gameboy/carts"
-FILES=("blue/pokeblue.gbc:blue.gbc" "red/pokered.gbc:red.gbc" "yellow/pokeyellow.gbc:yellow.gbc" "green/green.gb:green.gb")
-for FILE_MAPPING in "${FILES[@]}"; do
-    SOURCE_FILE="${FILE_MAPPING%%:*}"    # Get the source file (before ":")
-    TARGET_FILE="${FILE_MAPPING#*:}"    # Get the target file (after ":")
+FILES=("blue/pokeblue.gbc" "red/pokered.gbc" "yellow/pokeyellow.gbc" "green/green.gb")
+for SOURCE_FILE in "${FILES[@]}"; do
+    TARGET_FILE="${SOURCE_FILE##*/}"  # Get the target file (extract filename from path)
     aws s3 cp "$SOURCE_FILE" "s3://${S3_BUCKET}/$TARGET_FILE"
 done
