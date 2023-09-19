b0622e50a994855e7dd5d295d5bc696c5adfa532
Grant Cox <gcoxexcel@gmail.com>
2023-09-19 09:57:00 -0400

    new strat

--- .github/write_to_s3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-S3_BUCKET="letmedemo-assets/gameboy/carts"
-FILES=("blue/pokeblue.gbc" "red/pokered.gbc" "yellow/pokeyellow.gbc" "green/green.gb")
-for SOURCE_FILE in "${FILES[@]}"; do
-    TARGET_FILE="${SOURCE_FILE##*/}"  # Get the target file (extract filename from path)
-    aws s3 cp "$SOURCE_FILE" "s3://${S3_BUCKET}/$TARGET_FILE"
-done
