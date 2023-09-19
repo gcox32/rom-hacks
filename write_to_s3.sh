ee89bfba08ba78778ec4a33d999d11db87b8d65f
Grant Cox <gcoxexcel@gmail.com>
2023-09-19 06:29:58 -0400

    debug actions

--- write_to_s3.sh
+++ write_to_s3.sh
@@ -4,5 +4,6 @@ FILES=("blue/pokeblue.gbc:blue.gbc" "red/pokered.gbc:red.gbc" "yellow/pokeyellow
 for FILE_MAPPING in "${FILES[@]}"; do
     SOURCE_FILE="${FILE_MAPPING%%:*}"    # Get the source file (before ":")
     TARGET_FILE="${FILE_MAPPING#*:}"    # Get the target file (after ":")
-    aws s3 cp "$SOURCE_FILE" "s3://${S3_BUCKET}/$TARGET_FILE"
+    result=$(aws s3 cp "$SOURCE_FILE" "s3://${S3_BUCKET}/$TARGET_FILE")
+    echo "Result: $result"
 done
