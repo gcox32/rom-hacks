afe5ed632295be219cea9d72c72647d47425ae52
Grant Cox <gcoxexcel@gmail.com>
2023-09-19 09:32:20 -0400

    again

--- .github/write_to_s3.sh
+++ .github/write_to_s3.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 S3_BUCKET="letmedemo-assets/gameboy/carts"
 FILES=("blue/pokeblue.gbc" "red/pokered.gbc" "yellow/pokeyellow.gbc" "green/green.gb")
-aws configure list
 for SOURCE_FILE in "${FILES[@]}"; do
     TARGET_FILE="${SOURCE_FILE##*/}"  # Get the target file (extract filename from path)
     aws s3 cp "$SOURCE_FILE" "s3://${S3_BUCKET}/$TARGET_FILE"
